14b4c3e576b7edf1003a594d7262375c2769b10a
ANKIT (IIIT PRAYAGRAJ <ankitshankar027@gmail.com>
2025-07-13 21:00:12 +0530

    Create web3.ts

---
+++ shared/. server/server/ server/ client/src/. client/src/lib/web3.ts
@@ -0,0 +1,6 @@
+import { useState, useEffect } from 'react';
+import { connectWallet, getWalletBalance } from '@/lib/web3';
+
+export function useWeb3() {
+  // ... [Web3 hook for wallet management]
+}
